6b1d05e388304ea27a68e9c450aacccc82c33e7a
javayhu <javayhu@tencent.com>
2020-05-21 12:01:59 +0800

    move djinni into “tools” subfolder

--- common/run_djinni.sh
+++ common/run_djinni.sh
@@ -37,7 +37,7 @@ rm -rf $objc_out
 rm -rf $java_out 
 
 # execute the djinni command
-../djinni/src/run \
+../tools/djinni/src/run \
    --java-out $java_out \
    --java-package $java_package \
    --ident-java-field mFooBar \
