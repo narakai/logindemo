63967ec0395e42dab802d61e56e6394e12d1ae8d
ubuntu <ubuntu@localhost.localdomain>
2020-05-23 22:08:36 +0800

    run mysql docker container successfully

---
+++ server/mysql-service/README.md
@@ -0,0 +1,2 @@
+1. run docker-compose up
+2. view database in host:8080
---
+++ server/mysql-service/database/db.sql
@@ -0,0 +1,7 @@
+CREATE TABLE person (
+name varchar(25),
+password varchar(25)
+);
+
+INSERT INTO person(name, password) VALUES ('javayhu', 'javayhu');
+
