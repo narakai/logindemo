1da774a48b6ced07d1761c5da76da0c4d92dd50c
javayhu <javayhu@tencent.com>
2020-05-26 13:04:16 +0800

    build and install grpc & protoc in both dev and deploy host, then run these toosl from /usr/local/bin

--- protos/run_proto.sh
+++ protos/run_proto.sh
@@ -1,12 +1,16 @@
 
 #! /usr/bin/env bash
 
+######### 实际开发可以只用生成到当前目录，然后同步给客户端和服务器端 #########
+
 ### Configuration
 
 proto_file="helloworld.proto"
 
 # get base directory
 base_dir=$(cd "`dirname "0"`" && pwd)
+
+######### 生成到客户端 #########
 
 # output directories for generated src
 cpp_out="$base_dir/../client/android/app/src/main/cpp/protos"
@@ -19,11 +23,11 @@ mkdir $cpp_out
 mkdir $grpc_out
 
 # execute the protoc command
-../tools/grpc/cmake/build/third_party/protobuf/protoc \
+/usr/local/bin/protoc \
    --cpp_out=$cpp_out \
    $proto_file
 
-../tools/grpc/cmake/build/third_party/protobuf/protoc \
+/usr/local/bin/protoc \
    --plugin=protoc-gen-grpc=../tools/grpc/cmake/build/grpc_cpp_plugin \
    --grpc_out=$grpc_out \
    $proto_file
